f1069f034f5a12b8a3a6086dd866b93081dc66c3
lvhaiyu <lvhaiyu@espressif.com>
2025-02-27 23:29:54 +0800

    feat(p4): update code

--- examples/p4_eye_product_test/main/main.c
+++ examples/p4_eye_product_test/main/main.c
@@ -11,46 +11,32 @@
 
 #include "app_usb_msc.h"
 
-// #define TEST_DISK_PATH "/disk"
-// #define TEST_RESULT_FILE "test.txt"
+#define TEST_DISK_PATH "/disk"
+#define TEST_RESULT_FILE "test.txt"
 
 static const char *TAG = "main";
 
-const char *disk_path = "/disk";
-
-// static esp_err_t create_and_write_file(const char *path, char *data, bool append)
-// {
-//     ESP_LOGI(TAG, "Opening file %s", path);
-//     FILE *f = fopen(path, append ? "a" : "w");
-//     if (f == NULL) {
-//         ESP_LOGE(TAG, "Failed to open file for writing");
-//         return ESP_FAIL;
-//     }
-//     fprintf(f, "%s\n", data);
-//     fclose(f);
-//     ESP_LOGI(TAG, "File written");
-
-//     return ESP_OK;
-// }
-
-void create_and_write_file(const char *file_path, const char *content)
+static esp_err_t create_and_write_file(const char *path, char *data, bool append)
 {
-    FILE *f = fopen(file_path, "w");
+    ESP_LOGI(TAG, "Opening file %s", path);
+    FILE *f = fopen(path, append ? "a" : "w");
     if (f == NULL) {
         ESP_LOGE(TAG, "Failed to open file for writing");
-        return;
+        return ESP_FAIL;
     }
-    fprintf(f, "%s\n", content);
-    fclose(f); 
-    ESP_LOGI(TAG, "File written: %s", file_path);
+    fprintf(f, "%s\n", data);
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    return ESP_OK;
 }
 
 void app_main(void)
 {
-    // ESP_ERROR_CHECK(bsp_p4_eye_init());
+    ESP_ERROR_CHECK(bsp_p4_eye_init());
 
-    ESP_ERROR_CHECK(app_usb_msc_init(disk_path));
-    create_and_write_file("/disk/result.txt", "sssss");
+    ESP_ERROR_CHECK(app_usb_msc_init(TEST_DISK_PATH));
+    create_and_write_file(TEST_DISK_PATH "/" TEST_RESULT_FILE, "", false);
 
     // bsp_display_start();
     // bsp_display_lock(0);
@@ -63,13 +49,13 @@ void app_main(void)
     // bsp_display_unlock();
     // bsp_display_backlight_on();
 
-    // while(!app_usb_msc_stage()) {
+    while(!app_usb_msc_stage()) {
     //     lv_label_set_text(label, "Detecting USB HS...");
-    //     vTaskDelay(300 / portTICK_PERIOD_MS);
-    // }
+         vTaskDelay(300 / portTICK_PERIOD_MS);
+    }
 
     // lv_label_set_text(label, "USB HS detected");
-    // create_and_write_file(TEST_DISK_PATH "/" TEST_RESULT_FILE, "USB HS: PASS\n", true);
-    // vTaskDelay(1000 / portTICK_PERIOD_MS);
+    create_and_write_file(TEST_DISK_PATH "/" TEST_RESULT_FILE, "USB HS: PASS\n", true);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
 
