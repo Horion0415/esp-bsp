f853271071b15b153500f42de05c642c8332180b
lvhaiyu <lvhaiyu@espressif.com>
2025-03-02 16:41:27 +0800

    feat(p4): update code

--- examples/p4_eye_lvgl/main/main.c
+++ examples/p4_eye_lvgl/main/main.c
@@ -9,17 +9,113 @@
 #include "lvgl.h"
 #include "esp_log.h"
 
+static const char *TAG = "main";
+
+lv_obj_t * cont = NULL;
+
+static void scroll_event_cb(lv_event_t * e)
+{
+    lv_obj_t * cont = lv_event_get_target(e);
+
+    lv_area_t cont_a;
+    lv_obj_get_coords(cont, &cont_a);
+    lv_coord_t cont_y_center = cont_a.y1 + lv_area_get_height(&cont_a) / 2;
+
+    lv_coord_t r = lv_obj_get_height(cont) * 7 / 10;
+    uint32_t i;
+    uint32_t child_cnt = lv_obj_get_child_cnt(cont);
+    for(i = 0; i < child_cnt; i++) {
+        lv_obj_t * child = lv_obj_get_child(cont, i);
+        lv_area_t child_a;
+        lv_obj_get_coords(child, &child_a);
+
+        lv_coord_t child_y_center = child_a.y1 + lv_area_get_height(&child_a) / 2;
+
+        lv_coord_t diff_y = child_y_center - cont_y_center;
+        diff_y = LV_ABS(diff_y);
+
+        /*Get the x of diff_y on a circle.*/
+        lv_coord_t x;
+        /*If diff_y is out of the circle use the last point of the circle (the radius)*/
+        if(diff_y >= r) {
+            x = r;
+        }
+        else {
+            /*Use Pythagoras theorem to get x from radius and y*/
+            uint32_t x_sqr = r * r - diff_y * diff_y;
+            lv_sqrt_res_t res;
+            lv_sqrt(x_sqr, &res, 0x8000);   /*Use lvgl's built in sqrt root function*/
+            x = r - res.i;
+        }
+
+        /*Translate the item by the calculated X coordinate*/
+        lv_obj_set_style_translate_x(child, x, 0);
+
+        /*Use some opacity with larger translations*/
+        lv_opa_t opa = lv_map(x, 0, r, LV_OPA_TRANSP, LV_OPA_COVER);
+        lv_obj_set_style_opa(child, LV_OPA_COVER - opa, 0);
+    }
+}
+
+/**
+ * Translate the object as they scroll
+ */
+void lv_example_scroll_6(void)
+{
+    cont = lv_obj_create(lv_scr_act());
+    lv_obj_set_size(cont, 240, 240);
+    lv_obj_center(cont);
+    lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
+    lv_obj_add_event_cb(cont, scroll_event_cb, LV_EVENT_SCROLL, NULL);
+    lv_obj_set_style_radius(cont, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_style_clip_corner(cont, true, 0);
+    lv_obj_set_scroll_dir(cont, LV_DIR_VER);
+    lv_obj_set_scroll_snap_y(cont, LV_SCROLL_SNAP_CENTER);
+    lv_obj_set_scrollbar_mode(cont, LV_SCROLLBAR_MODE_OFF);
+
+    uint32_t i;
+    for(i = 0; i < 20; i++) {
+        lv_obj_t * btn = lv_btn_create(cont);
+        lv_obj_set_width(btn, lv_pct(100));
+
+        lv_obj_t * label = lv_label_create(btn);
+        lv_label_set_text_fmt(label, "Button %"LV_PRIu32, i);
+    }
+
+    /*Update the buttons position manually for first*/
+    lv_event_send(cont, LV_EVENT_SCROLL, NULL);
+
+    /*Be sure the fist button is in the middle*/
+    lv_obj_scroll_to_view(lv_obj_get_child(cont, 0), LV_ANIM_OFF);
+}
+
+static void btn_handler(void *arg, void *data)
+{
+    if((int)data == BSP_BUTTON_2) {
+        ESP_LOGI(TAG, "scroll up");
+        lv_obj_scroll_by(cont, 0, 50, LV_ANIM_ON);
+        lv_event_send(cont, LV_EVENT_SCROLL, NULL);
+    } else if((int)data == BSP_BUTTON_3) {
+        ESP_LOGI(TAG, "scroll down");
+        lv_obj_scroll_by(cont, 0, -50, LV_ANIM_ON);
+        lv_event_send(cont, LV_EVENT_SCROLL, NULL);
+    }
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(bsp_p4_eye_init());
 
+   /* Init Buttons */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[BSP_BUTTON_2], BUTTON_PRESS_DOWN, btn_handler, (void *) BSP_BUTTON_2));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[BSP_BUTTON_3], BUTTON_PRESS_DOWN, btn_handler, (void *) BSP_BUTTON_3));
+
     bsp_display_start();
     bsp_display_lock(0);
 
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, LV_PART_MAIN);
-    lv_label_set_text(label, "Hello, World!");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_example_scroll_6();
 
     bsp_display_unlock();
     bsp_display_backlight_on();
