3bda406c345b3a0906e06d41a12c8fc8133465f8
lvhaiyu <lvhaiyu@espressif.com>
2025-03-03 13:29:40 +0800

    feat(p4): update code

--- examples/p4_eye_lvgl/main/main.c
+++ examples/p4_eye_lvgl/main/main.c
@@ -22,23 +22,25 @@ lv_obj_t * cont = NULL;
 static void scroll_event_cb(lv_event_t * e)
 {
     lv_obj_t * cont = lv_event_get_target(e);
-
     lv_area_t cont_a;
     lv_obj_get_coords(cont, &cont_a);
     lv_coord_t cont_y_center = cont_a.y1 + lv_area_get_height(&cont_a) / 2;
 
+    // Calculate radius for circular motion effect
     lv_coord_t r = lv_obj_get_height(cont) * 7 / 10;
-    uint32_t i;
     uint32_t child_cnt = lv_obj_get_child_cnt(cont);
     
+    // Find the closest child to center
     lv_obj_t * closest_child = NULL;
     lv_coord_t min_diff = LV_COORD_MAX;
     
-    for(i = 0; i < child_cnt; i++) {
+    // Process each child button
+    for(uint32_t i = 0; i < child_cnt; i++) {
         lv_obj_t * child = lv_obj_get_child(cont, i);
         lv_area_t child_a;
         lv_obj_get_coords(child, &child_a);
 
+        // Calculate vertical distance from center
         lv_coord_t child_y_center = child_a.y1 + lv_area_get_height(&child_a) / 2;
         lv_coord_t diff_y = LV_ABS(child_y_center - cont_y_center);
         
@@ -47,42 +49,26 @@ static void scroll_event_cb(lv_event_t * e)
             closest_child = child;
         }
 
-        /*Get the x of diff_y on a circle.*/
-        lv_coord_t x;
-        /*If diff_y is out of the circle use the last point of the circle (the radius)*/
-        if(diff_y >= r) {
-            x = r;
-        }
-        else {
-            /*Use Pythagoras theorem to get x from radius and y*/
-            uint32_t x_sqr = r * r - diff_y * diff_y;
-            lv_sqrt_res_t res;
-            lv_sqrt(x_sqr, &res, 0x8000);   /*Use lvgl's built in sqrt root function*/
-            // x = r - res.i;
-            x = res.i - r;
-        }
+        // Calculate horizontal offset using circular motion
+        lv_sqrt_res_t sqrt_res;
+        lv_sqrt(r * r - diff_y * diff_y, &sqrt_res, 0x8000);
+        lv_coord_t x = (diff_y >= r) ? r : sqrt_res.i - r;
 
-        /*Translate the item by the calculated X coordinate*/
+        // Apply transformations
         lv_obj_set_style_translate_x(child, x, 0);
-
-        /*Use some opacity with larger translations*/
-        lv_opa_t opa = lv_map(x, 0, r, LV_OPA_TRANSP, LV_OPA_COVER);
-        lv_obj_set_style_opa(child, LV_OPA_COVER - opa, 0);
+        lv_obj_set_style_opa(child, LV_OPA_COVER - lv_map(x, 0, r, LV_OPA_TRANSP, LV_OPA_COVER), 0);
+        lv_obj_set_style_transform_zoom(child, 256, 0);
         
-        // reset the zoom and icon position
-        lv_obj_set_style_transform_zoom(child, 256, 0);  // reset to normal size
+        // Reset icon position
         lv_obj_t * img = lv_obj_get_child(child, 0);
         if(img) {
             lv_obj_align(img, LV_ALIGN_RIGHT_MID, 0, 0);
         }
     }
     
-    // only zoom the closest child
+    // Apply special effects to closest child
     if(closest_child) {
-        // zoom the selected child
-        lv_obj_set_style_transform_zoom(closest_child, 256 * ZOOM_FACTOR, 0);  // zoom to 140%
-        
-        // set the special position for the center button
+        lv_obj_set_style_transform_zoom(closest_child, 256 * ZOOM_FACTOR, 0);
         lv_obj_t * img = lv_obj_get_child(closest_child, 0);
         if(img) {
             lv_obj_set_pos(img, ZOOM_OFFSET, 0);
@@ -151,67 +137,57 @@ static void scroll_end_event_cb(lv_event_t * e)
  */
 void lv_example_scroll_6(void)
 {
+    // Create main container
     cont = lv_obj_create(lv_scr_act());
-    lv_obj_set_style_pad_row(cont, 20, 0);  
     lv_obj_set_size(cont, 240, 240);
-    // lv_obj_center(cont);
     lv_obj_set_pos(cont, -160, 0);
+    
+    // Set container properties
+    lv_obj_set_style_pad_row(cont, 20, 0);
     lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
-    lv_obj_add_event_cb(cont, scroll_event_cb, LV_EVENT_SCROLL, NULL);
-    lv_obj_add_event_cb(cont, scroll_end_event_cb, LV_EVENT_SCROLL_END, NULL);  // add scroll end event
     lv_obj_set_style_radius(cont, LV_RADIUS_CIRCLE, 0);
     lv_obj_set_style_clip_corner(cont, true, 0);
+    
+    // Configure scrolling behavior
     lv_obj_set_scroll_dir(cont, LV_DIR_VER);
     lv_obj_set_scroll_snap_y(cont, LV_SCROLL_SNAP_CENTER);
     lv_obj_set_scrollbar_mode(cont, LV_SCROLLBAR_MODE_OFF);
-
-    // set container opacity
-    lv_obj_set_style_bg_opa(cont, LV_OPA_10, 0);  // background opacity set to 70%
-    lv_obj_set_style_border_opa(cont, LV_OPA_TRANSP, 0);  // border opacity set to 50%
     
-    // optional: set container background color, make transparent effect more obvious
-    lv_obj_set_style_bg_color(cont, lv_color_hex(0xcccccc), 0);  // light gray background
+    // Add scroll event handlers
+    lv_obj_add_event_cb(cont, scroll_event_cb, LV_EVENT_SCROLL, NULL);
+    lv_obj_add_event_cb(cont, scroll_end_event_cb, LV_EVENT_SCROLL_END, NULL);
 
-    uint32_t i;
-    for(i = 0; i < 20; i++) {
+    // Set container visual style
+    lv_obj_set_style_bg_opa(cont, LV_OPA_10, 0);
+    lv_obj_set_style_border_opa(cont, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_bg_color(cont, lv_color_hex(0xcccccc), 0);
+
+    // Create buttons with alternating icons
+    for(uint32_t i = 0; i < 20; i++) {
         lv_obj_t * btn = lv_btn_create(cont);
         
-        // set button opacity
+        // Set button style - make all properties transparent
         lv_obj_set_style_bg_opa(btn, LV_OPA_TRANSP, 0);
         lv_obj_set_style_border_opa(btn, LV_OPA_TRANSP, 0);
         lv_obj_set_style_border_width(btn, 0, 0);
-        
-        // remove shadow effect
         lv_obj_set_style_shadow_width(btn, 0, 0);
-        lv_obj_set_style_shadow_spread(btn, 0, 0); 
+        lv_obj_set_style_shadow_spread(btn, 0, 0);
         lv_obj_set_style_shadow_opa(btn, LV_OPA_TRANSP, 0);
-        
-        // remove shadow offset
         lv_obj_set_style_shadow_ofs_x(btn, 0, 0);
         lv_obj_set_style_shadow_ofs_y(btn, 0, 0);
         
-        // set button width
         lv_obj_set_width(btn, lv_pct(100));
 
-        if(i % 2 == 0) {
-            lv_obj_t * img1 = lv_img_create(btn);
-            lv_img_set_src(img1, &camera_icon);
-            lv_obj_align(img1, LV_ALIGN_RIGHT_MID, 0, 0);
-            lv_img_set_zoom(img1, 140);
-            lv_img_set_size_mode(img1, LV_IMG_SIZE_MODE_REAL);
-        } else {
-            lv_obj_t * img1 = lv_img_create(btn);
-            lv_img_set_src(img1, &timer_icon);
-            lv_obj_align(img1, LV_ALIGN_RIGHT_MID, 0, 0);
-            lv_img_set_zoom(img1, 140);
-            lv_img_set_size_mode(img1, LV_IMG_SIZE_MODE_REAL);
-        }
+        // Create and configure icon
+        lv_obj_t * img = lv_img_create(btn);
+        lv_img_set_src(img, (i % 2 == 0) ? &camera_icon : &timer_icon);
+        lv_obj_align(img, LV_ALIGN_RIGHT_MID, 0, 0);
+        lv_img_set_zoom(img, 140);
+        lv_img_set_size_mode(img, LV_IMG_SIZE_MODE_REAL);
     }
 
-    /*Update the buttons position manually for first*/
+    // Initialize scroll position
     lv_event_send(cont, LV_EVENT_SCROLL, NULL);
-
-    /*Be sure the fist button is in the middle*/
     lv_obj_scroll_to_view(lv_obj_get_child(cont, 0), LV_ANIM_OFF);
 }
 
