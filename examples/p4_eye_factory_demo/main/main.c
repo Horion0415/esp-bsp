3cd98e13e7c1a3bbe8493c494237695615fcfda4
lvhaiyu <lvhaiyu@espressif.com>
2025-03-06 16:26:50 +0800

    feat(p4): update code

--- examples/p4_eye_factory_demo/main/main.c
+++ examples/p4_eye_factory_demo/main/main.c
@@ -17,9 +17,11 @@ static const char *TAG = "main";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Initialize the P4 Eye");
     ESP_ERROR_CHECK(bsp_p4_eye_init());
 
     // Initialize the display
+    ESP_LOGI(TAG, "Initialize the display");
     bsp_display_start();
 
     bsp_display_lock(0);
@@ -29,14 +31,16 @@ void app_main(void)
     bsp_display_backlight_on();
 
     // Initialize the application control module
+    ESP_LOGI(TAG, "Initialize the application control module");
     ESP_ERROR_CHECK(app_control_init());
 
     // Initialize the I2C
+    ESP_LOGI(TAG, "Initialize the I2C");
     i2c_master_bus_handle_t i2c_handle;
-
     ESP_ERROR_CHECK(bsp_i2c_init());
     bsp_get_i2c_bus_handle(&i2c_handle);
 
     // Initialize the video streaming application
+    ESP_LOGI(TAG, "Initialize the video streaming application");
     ESP_ERROR_CHECK(app_video_stream_init(i2c_handle));
 }
