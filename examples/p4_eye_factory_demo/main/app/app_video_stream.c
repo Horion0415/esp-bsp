31feca53a1976ce99c5bd0bbc4ebb900243289f4
lvhaiyu <lvhaiyu@espressif.com>
2025-03-07 22:15:21 +0800

    feat(p4): update code

--- examples/p4_eye_factory_demo/main/app/app_video_stream.c
+++ examples/p4_eye_factory_demo/main/app/app_video_stream.c
@@ -387,7 +387,7 @@ static void camera_video_frame_operation(uint8_t *camera_buf, uint8_t camera_buf
     lv_refr_now(NULL);
     bsp_display_unlock();
 
-    if(is_take_photo && ui_extra_get_current_page() == UI_PAGE_CAMERA) {
+    if(is_take_photo && (ui_extra_get_current_page() == UI_PAGE_CAMERA || ui_extra_get_current_page() == UI_PAGE_INTERVAL_CAM)) {
         photo_task_params_t params = {
             .camera_buf = camera_buf,
             .width = camera_buf_hes,
