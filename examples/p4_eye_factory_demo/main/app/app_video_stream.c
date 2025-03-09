23592f9dc9a46983b3ff850604f5290205a289ee
lvhaiyu <lvhaiyu@espressif.com>
2025-03-09 17:09:49 +0800

    feat(p4): update code

--- examples/p4_eye_factory_demo/main/app/app_video_stream.c
+++ examples/p4_eye_factory_demo/main/app/app_video_stream.c
@@ -68,7 +68,6 @@ static void enter_deep_sleep(uint16_t sleep_minutes)
     
     // Save the interval photo state
     app_storage_save_interval_state(is_interval_photo_active, next_wake_time);
-    app_storage_save_photo_count(app_extra_get_saved_photo_count());
 
     // Initialize the sleep IO
     bsp_sleep_io_init();
