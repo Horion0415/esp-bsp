403f32d32e1da30174dbb2ae053231f26e287692
lvhaiyu <lvhaiyu@espressif.com>
2025-06-05 10:15:19 +0800

    feat(p4): update code

--- examples/p4_eye_product_test/main/app/app_gpio.h
+++ examples/p4_eye_product_test/main/app/app_gpio.h
@@ -10,12 +10,12 @@ extern "C" {
 /**
  * @brief Number of GPIO pins in group 1
  */
-#define GROUP1_PIN_NUM 5
+#define GROUP1_PIN_NUM 6
 
 /**
  * @brief Number of GPIO pins in group 2  
  */
-#define GROUP2_PIN_NUM 5
+#define GROUP2_PIN_NUM 6
 
 /**
  * @brief GPIO pins in group 1 (used as sender and receiver)
